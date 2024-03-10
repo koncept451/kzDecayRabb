6e3cd31de30aa603f33930b8b12d598a6f0cd68e
Julian Almeida <jalmeida720@gmail.com>
2024-03-10 12:29:07 -0400

    Added the rest of MSP base items

--- Scripts/4_World/mspDecay.c
+++ Scripts/4_World/mspDecay.c
@@ -11,4 +11,102 @@ modded class Msp_Openable_Base
     {
         return 24.0 * 60.0 * 60.0;
     }
-}
+};
+modded class Msp_Item
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_Openable_Placeable_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_Greenhouse_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_Planter_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_Storage_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_AmmoStorage_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class Msp_InventoryStorage_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};

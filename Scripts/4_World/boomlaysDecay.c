0a8245e8fb070340b202e9a2110bbca8225ac286
Julian Almeida <jalmeida720@gmail.com>
2024-03-16 06:30:46 -0400

    added Boomlay's Things, restructed Storage Containers
    Somehow I missed the custom base classes in Storage Containers, forgive me, its been fixed. No longer uses Container_Base

---
+++ Scripts/4_World/boomlaysDecay.c
@@ -0,0 +1,104 @@
+#ifdef bl_shared_data
+
+modded class bl_placeable
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+modded class bl_thing
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+modded class bl_bed
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+modded class bl_large_depot
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+modded class bl_deposit_container
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class bl_deposit_container
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class bl_stove_barrel
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class bl_repair_anvil
+{
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+#endif
--- Scripts/4_World/storageContainersDecay.c
+++ Scripts/4_World/storageContainersDecay.c
@@ -1,14 +1,124 @@
 #ifdef SFM_Storage_Containers
 
-modded class Container_Base
+modded class SFM_Container_Junk_ColorBase
 {
-      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
     override float GetCostToUpkeep()
     {
         return 0.00001;
     }
 
     // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Ammo_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Food_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Grenade_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Items_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Items_THICC_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Magazine_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Medicine_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class SFM_Container_Money_ColorBase
+{
+    //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
     override float GetTimeToDecay()
     {
         return 24.0 * 60.0 * 60.0;

46a2b6eb750a95d863cc60bd14172696558edb3b
Julian Almeida <jalmeida720@gmail.com>
2024-03-19 23:43:48 -0400

    added pitstop1 & 2

---
+++ Scripts/4_World/pitstopDecay.c
@@ -0,0 +1,190 @@
+#ifdef pitstop_stuff
+
+modded class PITSTOP_GardenPot
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_WoodenChest
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_GunLocker
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_GunRack
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_Shelves_Large
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_Shelves_Small
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_WoodenGunRack01
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP_WoodenGunRack02
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+#endif
+#ifdef pitstop2_stuff
+
+modded class PITSTOP2_BigSafe
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP2_Couch_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP2_RailgunBox
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP2_TallSafe
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+modded class PITSTOP2_BigTeddy_Base
+{
+      //how many nails per day will it consume | it needs to be higher than 0 to decay (optimization to prevent it from unnecessarily decaying all items)
+    override float GetCostToUpkeep()
+    {
+        return 0.00001;
+    }
+
+    // time until it decays full in seconds | default is 24 hours
+    override float GetTimeToDecay()
+    {
+        return 24.0 * 60.0 * 60.0;
+    }
+};
+
+#endif
